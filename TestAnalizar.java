07602e62945e0adea6ffc55b2806752d1a20c4ad
laft-flyzx <laft.flyzx@gmail.com>
2016-01-19 22:01:19 -0600

    Test 3
    Test 3

--- TestAnalizar.java
+++ TestAnalizar.java
@@ -3,22 +3,52 @@
 *@version 0.1
 */
 import util.analizar.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 /**
 *Clase Testea AnalizaArray
 */
 public class TestAnalizar{
   /**
+  *@param String[]: arreglo con las matriculas, String: tipo de ordenacion
+  */
+  private static void guardarMatriculas(String[]matriculas, String tipo){
+    String ruta = "Datos/";
+    String texto = "";
+    int num = matriculas.length;
+    for (int i=0; i<num; i++) {
+      texto += matriculas[i]+"\n";
+    }
+    try{
+      File archivo = new File(ruta+tipo+num+".txt");
+      if(! archivo.exists()) {
+        BufferedWriter bw;
+        bw = new BufferedWriter(new FileWriter(archivo));
+        bw.write(texto);
+        bw.close();
+      }
+    }catch(Exception e){}
+  }
+  /**
+  *@param: int: numero de matriculas a generar
+  */
+  private static void generarMatriculasDesordenadas(int num){
+    GenerarMatriculas gm = new GenerarMatriculas();
+    String[] matriculas = gm.generar(num);
+    guardarMatriculas(matriculas, "Des");
+    AnalizaArray aa = new AnalizaArray(matriculas);
+    System.out.println(aa.calculoFuncion());
+  }
+  /**
   *metodo main
   *@param arreglo de datos de tipo String
   */
   public static void main(String[] args) {
-
-    GenerarMatriculas gm = new GenerarMatriculas();
-    String[] matriculas = gm.generar(100);
-    for (int i=0; i<matriculas.length; i++) {
-      System.out.println(matriculas[i]);
-    }
-    AnalizaArray aa = new AnalizaArray(matriculas);
-    System.out.println(aa.calculoFuncion());
+    generarMatriculasDesordenadas(10);
+    generarMatriculasDesordenadas(50);
+    generarMatriculasDesordenadas(100);
+    generarMatriculasDesordenadas(500);
   }
 }
