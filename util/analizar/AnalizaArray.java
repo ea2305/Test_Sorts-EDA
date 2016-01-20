6c672c1f8c8e61197da1c38482340ea09543f28d
laft-flyzx <laft.flyzx@gmail.com>
2016-01-19 20:06:09 -0600

    Test 2
    Architur fuir doi prexint

--- util/analizar/AnalizaArray.java
+++ util/analizar/AnalizaArray.java
@@ -43,17 +43,103 @@ public class AnalizaArray{
     return (int)(Math.log(valor) / Math.log(base));
   }
   /**
+  *@param int: valor
+  *@return boolean: signo + = true, signo - = false
+  */
+  private boolean signo(int a){
+    if(a < 0) return false;
+    else return true;
+  }
+  /**
+  *@param int: valor
+  *@return int: valor absoluto
+  */
+  private int valAbs(int a){
+    if(a < 0) return -a;
+    else return a;
+  }
+  /**
+  *@return boolean[]: indica si esta ordenado y su tipo
+  */
+  private boolean[] ordenado(){
+    boolean[] orden = {false, false};
+    if(this.data_Sort != null){
+      int n = getNumElementos();
+      int box = data_Sort[0].compareTo(data_Sort[n-1]);
+      int distancia = valAbs(box);
+      orden[1] = signo(box);
+      for (int i=n-1; i>0; i/=2) {
+        box = data_Sort[0].compareTo(data_Sort[i]);
+        if(orden[1] != signo(box)){
+          return orden;
+        }
+        if(valAbs(distancia) < valAbs(box)){
+          return orden;
+        }
+        distancia = box;
+      }
+      orden[0]=true;
+      return orden;
+    }
+    if(this.data_SortInteger != null){
+      int n = getNumElementos();
+      int box = data_SortInteger[0].compareTo(data_SortInteger[n-1]);
+      int distancia = valAbs(box);
+      orden[1] = signo(box);
+      for (int i=n-1; i>0; i/=2) {
+        box = data_SortInteger[0].compareTo(data_SortInteger[i]);
+        if(orden[1] != signo(box)){
+          return orden;
+        }
+        if(valAbs(distancia) < valAbs(box)){
+          return orden;
+        }
+        distancia = box;
+      }
+      orden[0]=true;
+      return orden;
+    }
+    return orden;
+  }
+  /**
   *@return String: calculo de complejidad con funcion
   */
   public String calculoFuncion(){
     int n = getNumElementos();
-    return(
-      "\n"+"Calculo de complejidad por funcion"
-      +"\n\t"+"HeapSort complejidad: "+ (n*log(n,2))
-      +"\n\t"+"Insercion complejidad: "+ (n*n)
-      +"\n\t"+"Merge complejidad: "+ (n*log(n,2))
-      +"\n\t"+"Seleccion complejidad: "+ (n*n)
-      +"\n\t"+"Quicksort complejidad: "+ (n*log(n,2))
-    );
+    boolean[] orden = ordenado();
+    if(orden[0]){
+      if(orden[1]){
+        return(
+          "\n"+"Calculo de complejidad por funcion"
+          +"\n"+"Arreglo Ordenado Ascendente"
+          +"\n\t"+"HeapSort complejidad: "+ (n*log(n,2))
+          +"\n\t"+"Insercion complejidad: "+ (n*n)
+          +"\n\t"+"Merge complejidad: "+ (n*log(n,2))
+          +"\n\t"+"Seleccion complejidad: "+ (n*n)
+          +"\n\t"+"Quicksort complejidad: "+ (n*log(n,2))
+        );
+      }else{
+        return(
+          "\n"+"Calculo de complejidad por funcion"
+          +"\n"+"Arreglo Ordenado Descendente"
+          +"\n\t"+"HeapSort complejidad: "+ (n*log(n,2))
+          +"\n\t"+"Insercion complejidad: "+ (n*n)
+          +"\n\t"+"Merge complejidad: "+ (n*log(n,2))
+          +"\n\t"+"Seleccion complejidad: "+ (n*n)
+          +"\n\t"+"Quicksort complejidad: "+ (n*log(n,2))
+        );
+      }
+
+    }else{
+      return(
+        "\n"+"Calculo de complejidad por funcion"
+        +"\n"+"Arreglo Desordenado"
+        +"\n\t"+"HeapSort complejidad: "+ (n*log(n,2))
+        +"\n\t"+"Insercion complejidad: "+ (n*n)
+        +"\n\t"+"Merge complejidad: "+ (n*log(n,2))
+        +"\n\t"+"Seleccion complejidad: "+ (n*n)
+        +"\n\t"+"Quicksort complejidad: "+ (n*log(n,2))
+      );
+    }
   }
 }
